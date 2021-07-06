339c0b35c7f739269dafaa5ee9c13d404adea4ec
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-06 19:40:34 +0200

    wip

--- api/adopters/adopters-router.js
+++ api/adopters/adopters-router.js
@@ -33,8 +33,8 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.get('/:id/dogs', (req, res) => {
-  
+router.get('/:id/dogs', async (req, res) => {
+  console.log('get dogs by adopter')
 });
 
 router.post('/', (req, res) => {
